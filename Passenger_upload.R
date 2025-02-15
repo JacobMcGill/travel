c576a815df90d2eb1b492454e29f2458df9e4e8e
JacobMcGill <jacobpmcgill97@gmail.com>
2025-02-15 17:24:39 -0600

    Added passenger upload

--- Passenger_upload.R
+++ Passenger_upload.R
@@ -1,14 +1,22 @@
 library(tidyverse)
 library(Hmisc)
 #Importing files
-setwd("C://Users/jacob/OneDrive/Documents/travel/Airport_Origin")
+setwd("C://Users/jacob/OneDrive/Documents/travel")
 airport_files = c("WDI.csv","RRDC.csv", "TI.csv","STI.csv","SF.csv", "SD.csv", "SLC.csv", "PHX.csv", 
                   "PHI.csv", "ORL.csv", "NLI.csv", "LGA.csv", "JFK.csv", "NAS.csv",
                   "MIA.csv", "LA.csv", "LV.csv", "ATL.csv", "FrtLd_Holly.csv", "CHI_Hare.csv", "CHI_Midway.csv", 
-                  "ATX.csv")
+                  "ATX.csv", "BA_WA.csv", "LI.csv", 
+                  "CDI.csv", "DFW.csv","DI.csv", 
+                  "DMWC.csv", "GBIH.csv", "MSPI.csv"
+                  
+)
 airport_data = read_csv(airport_files, id = "file", skip = 1) %>%
   mutate(airport_origin = substr(file, 1, nchar(file) - 4),
-         Year = as.factor(Year))
+         Year = as.factor(Year),
+         ) %>%
+  rename(Domestic_Passengers = DOMESTIC,
+         International_Passengers = INTERNATIONAL,
+         Total_Passenger = TOTAL)
 airport_data
 #Note: The dataframe has both monthly values and total values for each year, so I qill seperate them
 airport_total = airport_data %>%
